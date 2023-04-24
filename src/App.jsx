1728d20c915148ded94989dd6957a390aaaf8d12
teojiesern <teojiesern42@gmail.com>
2023-04-24 10:54:16 +0800

    finish setting up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Content  from "./components/Content"
+import Sidebar from "./components/SideBar"
+
+export default function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Content />
+    </div>
+  )
+}
