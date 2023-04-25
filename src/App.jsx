9fe9d2595605c96b2b0067d802b5ff40072dcb62
teojiesern <teojiesern42@gmail.com>
2023-04-25 11:00:52 +0800

    completed sidebar mvp

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,30 @@ import Content  from "./components/Content"
 import Sidebar from "./components/SideBar"
 
 export default function App() {
+  const [notes, setNotes] = React.useState([])
+  const [currentId, setCurrentId] = React.useState(0)
+
+  function createNote(){
+    const newNote = {
+      id: notes.length+1,
+      body:"Welcome to new notes..."
+    }
+    changeCurrentId(newNote.id)
+    setNotes(prevNotes => {
+      return ([newNote, ...prevNotes])
+    })
+  }
+
+  function changeCurrentId(newId){
+    setCurrentId(newId)
+  }
+
   return (
     <div>
-      <Sidebar />
+      <Sidebar 
+        notes={notes}
+        createNote={createNote}
+        currentId={currentId}/>
       <Content />
     </div>
   )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,21 @@
 import React from "react"
 
-export default function Sidebar(){
-    
+export default function Sidebar(props){
+    const [newNoteAdded, setNewNoteAdded] = useState(false);
+    const noteElements = props.notes.map(note => (
+        <div key={note.id} className={note.id === props.currentId ? "selected-note" : "non-selected-note"}>
+            {note.body}
+        </div>
+    ))
+    return(
+        <div className="sidebar">
+            <div className="sidebar-title">
+                <h1>Notes</h1>
+                <button onClick={props.createNote}>+</button>
+            </div>
+            <div>
+                {noteElements}
+            </div>
+        </div>
+    )
 }
